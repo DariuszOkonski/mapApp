878a3fa1324457b60094d00d469a8da8a462a98f
Dariusz Okonski <darek200180@gmail.com>
2022-05-12 18:33:50 +0100

    add data

--- src/User.ts
+++ src/User.ts
@@ -8,10 +8,10 @@ export class User {
     }
 
     constructor() {
-        this.name = `${faker.name.firstName} ${faker.name.lastName}`,
+        this.name = `${faker.name.firstName()} ${faker.name.lastName()}`,
         this.location = {
-            lat: Number(faker.address.latitude),
-            lng: Number(faker.address.longitude)
+            lat: parseFloat(faker.address.latitude()),
+            lng: parseFloat(faker.address.longitude())
         } 
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
-console.log('welcome')
+import { User } from "./User"
+
+const user = new User();
+
+console.log(user)
