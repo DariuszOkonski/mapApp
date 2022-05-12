4d134c8e9169ceae2d4928b026d2f63ffaacce5b
Dariusz Okonski <darek200180@gmail.com>
2022-05-12 18:14:22 +0100

    add properties and constructor to User

--- src/User.ts
+++ src/User.ts
@@ -1,3 +1,17 @@
+import faker from 'faker';
+
 export class User {
-    
+    name: string;
+    location: {
+        lat: number;
+        lng: number;
+    }
+
+    constructor() {
+        this.name = `${faker.name.firstName} ${faker.name.lastName}`,
+        this.location = {
+            lat: Number(faker.address.latitude),
+            lng: Number(faker.address.longitude)
+        } 
+    }
 }
