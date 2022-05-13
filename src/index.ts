d929a6f16f520827095b93d218600bd86a9ae1a7
Dariusz Okonski <darek200180@gmail.com>
2022-05-13 07:25:56 +0100

    add map to html

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import { User } from "./User"
 import { Company } from "./Company"
 
-const user = new User();
-const company = new Company(); 
 
-console.log(user);
-console.log(company);
+const map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 1.5,
+    center: {
+        lat: 0,
+        lng: 0
+    }
+});
